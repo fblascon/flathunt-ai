845a53164554db4ab3d730524373852500f595f0
Fernando Blasco <fblascon@gmail.com>
2026-06-01 17:21:54 +0200

    fix: update supabase anon keys (JWT format)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://quipyyrmhzbcxlksthpo.supabase.co',
-  supabaseAnonKey: 'sb_publishable_Xuh7__uy910YRq1BNZXezw_Dl-zQoL3',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1aXB5eXJtaHpiY3hsa3N0aHBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDQ0OTYsImV4cCI6MjA5NTI4MDQ5Nn0.0PcitA6FtXkdEZSF07lMIdtZ2hJeR7z29Doms4ax_xI',
   apiUrl: 'https://flathunt-ai.vercel.app/api',
 };
